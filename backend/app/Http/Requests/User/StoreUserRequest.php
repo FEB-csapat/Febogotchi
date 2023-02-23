c52345b6ca3c1a2836f2ed36b72b569799abdb74
Isakov <isakov.erik20021@gmail.com>
2023-02-23 11:58:44 +0100

    Refactor controllers

--- backend/app/Http/Requests/User/StoreUserRequest.php
+++ backend/app/Http/Requests/User/StoreUserRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\User;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreRegisterRequest extends FormRequest
+class StoreUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,8 +24,9 @@ class StoreRegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            "name" => "required|min:4|max:20|unique:users,name",
-            "password" => "required|min:5|max:255"
+            'pet_id' => "numeric",
+            'name' => "required|min:2|max:20",
+            'password' => "required|min:2|max:20",
         ];
     }
 }
